e150acbb0fd1a6a2ea567609f1212f645d669b82
klebs <none>
2021-12-12 17:30:27 -0500

    minor updates

---
+++ surge-math/src/limit_range.rs
@@ -0,0 +1,64 @@
+ix!();
+
+pub trait LimitRange: Sized {
+
+    fn limit_range(self, low: Self, hi: Self) -> Self;
+
+    /**
+      | provides a possibility for taking a
+      | non-simd path. if we don't customize it,
+      | the ordinary simd code will be called if it
+      | is used in "limit_range"
+      |
+      */
+    #[inline] fn limit_range_nosimd(self, low: Self, hi: Self) -> Self {
+        self.limit_range(low,hi)
+    }
+}
+
+pub fn limit_range<T: LimitRange>(x: T, low: T, high: T) -> T {
+    x.limit_range(low,high)
+}
+
+impl LimitRange for f32 {
+
+    #[cfg(target_arch = "x86_64")] #[inline] 
+    fn limit_range(self, low: Self, high: Self) -> Self {
+
+        let mut result: f32 = 0.0;
+
+        unsafe {
+            _mm_store_ss(&mut result,
+            _mm_min_ss(_mm_max_ss(_mm_load_ss(&self), _mm_load_ss(&low)), _mm_load_ss(&high))) 
+        };
+
+        result
+    }
+    fn limit_range_nosimd(self, min: f32, max: f32) -> f32 {
+        std::cmp::max(std::cmp::min(FloatOrd(self),FloatOrd(max)), FloatOrd(min)).0
+    }
+}
+
+impl LimitRange for i32 {
+
+    fn limit_range(self, l: i32, h: i32) -> i32 
+    {
+        std::cmp::max(std::cmp::min(self,h), l)
+    }
+}
+
+impl LimitRange for u32 {
+    fn limit_range(self, l: u32, h: u32) -> u32 
+    {
+        std::cmp::max(std::cmp::min(self,h), l)
+    }
+}
+
+
+impl LimitRange for f64 {
+    fn limit_range(self, min:f64, max:f64) -> f64 
+    {
+        std::cmp::max(std::cmp::min(FloatOrd(self),FloatOrd(max)), FloatOrd(min)).0
+    }
+}
+
