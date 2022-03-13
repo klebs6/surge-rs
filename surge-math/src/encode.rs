db2840ba1ff6eb3fac5be000ed7f6fc371f7d475
klebs <none>
2022-03-12 19:35:25 -0500

    updated file /Users/kleb/bethesda/work/repo/surge-rs/surge-math/src/encode.rs

--- surge-math/src/encode.rs
+++ surge-math/src/encode.rs
@@ -1,47 +1,80 @@
 ix!();
 
-///# Safety
-///
-///ensure we can access nquads * 4 values from both l and r
-pub unsafe fn encode_mid_side<NQ>(l: *mut f32, r: *mut f32, m: *mut f32, s: *mut f32, nquads: NQ)
-where 
-    <NQ as TryInto<usize>>::Error: Debug, 
-    NQ: TryInto<usize>
-{
-    let nquads: usize = nquads.try_into().unwrap();
-
-    for i in (0..nquads).step_by(4)
-    {
-        *(m as *mut __m128).add(i)     = _mm_mul_ps(_mm_add_ps(*(l as *mut __m128).add(i),     *(r as *mut __m128).add(i)), m128_half![]);
-        *(s as *mut __m128).add(i)     = _mm_mul_ps(_mm_sub_ps(*(l as *mut __m128).add(i),     *(r as *mut __m128).add(i)), m128_half![]);
-        *(m as *mut __m128).add(i + 1) = _mm_mul_ps(_mm_add_ps(*(l as *mut __m128).add(i + 1), *(r as *mut __m128).add(i + 1)), m128_half![]);
-        *(s as *mut __m128).add(i + 1) = _mm_mul_ps(_mm_sub_ps(*(l as *mut __m128).add(i + 1), *(r as *mut __m128).add(i + 1)), m128_half![]);
-        *(m as *mut __m128).add(i + 2) = _mm_mul_ps(_mm_add_ps(*(l as *mut __m128).add(i + 2), *(r as *mut __m128).add(i + 2)), m128_half![]);
-        *(s as *mut __m128).add(i + 2) = _mm_mul_ps(_mm_sub_ps(*(l as *mut __m128).add(i + 2), *(r as *mut __m128).add(i + 2)), m128_half![]);
-        *(m as *mut __m128).add(i + 3) = _mm_mul_ps(_mm_add_ps(*(l as *mut __m128).add(i + 3), *(r as *mut __m128).add(i + 3)), m128_half![]);
-        *(s as *mut __m128).add(i + 3) = _mm_mul_ps(_mm_sub_ps(*(l as *mut __m128).add(i + 3), *(r as *mut __m128).add(i + 3)), m128_half![]);
-    }
+#[inline] unsafe fn access(x: *mut f32, idx: usize) -> *mut __m128 {
+    (x as *mut __m128).add(idx)
 }
 
 ///# Safety
 ///
 ///ensure we can access nquads * 4 values from both l and r
-pub unsafe fn decode_mid_side<NQ>(m: *mut f32, s: *mut f32, l: *mut f32, r: *mut f32, nquads: NQ) 
+pub unsafe fn encode_mid_side<NQ>(
+    l: *mut f32, 
+    r: *mut f32, 
+    m: *mut f32, 
+    s: *mut f32, 
+    nquads: NQ)
+
 where 
     <NQ as TryInto<usize>>::Error: Debug, 
     NQ: TryInto<usize>
 {
     let nquads: usize = nquads.try_into().unwrap();
 
+    let half = m128_half![];
+
+    let encode = |offset: usize| {
+
+        let m = access(m,offset);
+        let s = access(s,offset);
+        let r = access(r,offset);
+        let l = access(l,offset);
+
+        *m = _mm_mul_ps(_mm_add_ps(*l, *r), half);
+        *s = _mm_mul_ps(_mm_sub_ps(*l, *r), half);
+    };
+
     for i in (0..nquads).step_by(4)
     {
-        *(l as *mut __m128).add(i)     = _mm_add_ps(*(m as *mut __m128).add(i),     *(s as *mut __m128).add(i));
-        *(r as *mut __m128).add(i)     = _mm_sub_ps(*(m as *mut __m128).add(i),     *(s as *mut __m128).add(i));
-        *(l as *mut __m128).add(i + 1) = _mm_add_ps(*(m as *mut __m128).add(i + 1), *(s as *mut __m128).add(i + 1));
-        *(r as *mut __m128).add(i + 1) = _mm_sub_ps(*(m as *mut __m128).add(i + 1), *(s as *mut __m128).add(i + 1));
-        *(l as *mut __m128).add(i + 2) = _mm_add_ps(*(m as *mut __m128).add(i + 2), *(s as *mut __m128).add(i + 2));
-        *(r as *mut __m128).add(i + 2) = _mm_sub_ps(*(m as *mut __m128).add(i + 2), *(s as *mut __m128).add(i + 2));
-        *(l as *mut __m128).add(i + 3) = _mm_add_ps(*(m as *mut __m128).add(i + 3), *(s as *mut __m128).add(i + 3));
-        *(r as *mut __m128).add(i + 3) = _mm_sub_ps(*(m as *mut __m128).add(i + 3), *(s as *mut __m128).add(i + 3));
+        encode(i);
+        encode(i + 1);
+        encode(i + 2);
+        encode(i + 3);
+    }
+}
+
+///# Safety
+///
+///ensure we can access nquads * 4 values from
+///both l and r
+pub unsafe fn decode_mid_side<NQ>(
+    m: *mut f32, 
+    s: *mut f32, 
+    l: *mut f32, 
+    r: *mut f32, 
+    nquads: NQ) 
+
+where 
+    <NQ as TryInto<usize>>::Error: Debug, 
+    NQ: TryInto<usize>
+{
+    let nquads: usize = nquads.try_into().unwrap();
+
+    let decode = |offset: usize| {
+
+        let m = access(m,offset);
+        let s = access(s,offset);
+        let r = access(r,offset);
+        let l = access(l,offset);
+
+        *l = _mm_add_ps(*m, *s);
+        *r = _mm_sub_ps(*m, *s);
+    };
+
+    for i in (0..nquads).step_by(4)
+    {
+        decode(i);
+        decode(i + 1);
+        decode(i + 2);
+        decode(i + 3);
     }
 }
